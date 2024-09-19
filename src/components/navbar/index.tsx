494df1a3f92647dc96c061bf1bab50561959d64c
Samba Gandega <103249282+sambagdg@users.noreply.github.com>
2024-09-19 15:14:43 +0200

    portfolio link update

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,6 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <h1 className="page-title">SmBFilms</h1>
+      <a className="portfolio_link" href="https://gandegasamba.vercel.app/">Mon Portfolio</a>
     </nav>
   );
 }
